27d32b6cff64f49edf380158efc1e4199e0255b9
임채연 <dlacodus0407@gmail.com>
2024-11-11 21:24:56 +0900

    test(app): 예외 테스트를 추가합니다

--- src/test/java/store/ApplicationTest.java
+++ src/test/java/store/ApplicationTest.java
@@ -61,6 +61,22 @@ class ApplicationTest extends NsTest {
         });
     }
 
+    @Test
+    void 존재하지_않는_상품을_입력하면_예외() {
+        assertSimpleTest(() -> {
+            runException("[컵면-12]");
+            assertThat(output()).contains("[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요.");
+        });
+    }
+
+    @Test
+    void 올바른_형식으로_입력하지_않으면_예외() {
+        assertSimpleTest(() -> {
+            runException("-12]");
+            assertThat(output()).contains("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
+        });
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
